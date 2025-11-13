e735c83420fb0c3c73e9415f578b666b98c5a0ff
amarjeet720 <amarjeetkumar17dc@gmail.com>
2025-11-13 11:06:44 +0530

    Normalize line endings

---
+++ frontend/jeeva-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Component/Navbar'
+
+const App = () => {
+  return (
+    <div className='font-sm text-black'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/jeeva-frontend/src/Component/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='font-sm text-red'>
+        <h1></h1>
+    </div>
+  )
+}
+
+export default Navbar
